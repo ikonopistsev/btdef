59b3f39133a3ac2f11fd375a156591fb1aaeb3b7
meil <1@1.1>
2022-05-25 12:40:48 +0300

    remove to_string

--- include/btdef/conv.hpp
+++ include/btdef/conv.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "btdef/conv/to_text.hpp"
-#include "btdef/conv/to_string.hpp"
 #include "btdef/conv/to_hex.hpp"
 #include "btdef/conv/to_hex_text.hpp"
 #include "btdef/conv/string_traits.hpp"
@@ -9,7 +8,6 @@
 namespace btdef {
 
 using btdef::conv::to_text;
-using btdef::conv::to_string;
 using btdef::conv::to_hex;
 using btdef::conv::to_hex00;
 
